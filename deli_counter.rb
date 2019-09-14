2b1675f81982eafb8996a1966ee8aa53aa65611e
Lialah Barrer <lrbarrer@gmail.com>
2019-09-14 18:29:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 # Write your code here.
 def line(deli)
   if deli.empty?
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     puts empyt
   end
 end
 
 def take_a_nember
+  
+end
+
+def now_serving
   
 end
